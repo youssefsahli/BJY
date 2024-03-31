8e2fb2c6bae916e818b317ad6f016497dfc75552
Youssef Sahli <sahli.youssef@ymail.com>
2024-03-31 05:33:53 +0200

    Add game classes and grid

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -2,43 +2,134 @@
  * Y. Sahli GPLv3
  * Main Canvas File
  */
+function windowToCanvas(canvas, x, y) {
+	var bbox = canvas.getBoundingClientRect();
+	return {
+		x: x - bbox.left * (canvas.width / bbox.width),
+		y: y - bbox.top * (canvas.height / bbox.height)
+	};
+}
+
+class Vec2 {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	static ZERO() {
+		return new Vec2(0, 0);
+	}
+
+	static ONE() {
+		return new Vec2(1, 1);
+	}
+
+	times(n) {
+		return new Vec2(this.x * n, this.y * n);
+	}
+
+	divide(n) {
+		return new Vec2(this.x / n, this.y / n);
+	}
+
+	add(v) {
+		return new Vec2(this.x + v.x, this.y + v.y);
+	}
+
+	sub(v) {
+		return new Vec2(this.x - v.x, this.y - v.y);
+	}
+
+}
+
+class CanvasObject extends Vec2 {
+	constructor(x, y) {
+		super(x, y);
+	}
+}
+
+class Brick extends CanvasObject {
+	width = 71;
+	height = 36;
+	padding = 5;
+	color = '#e01b24';
+
+	constructor(x, y) {
+		super(x, y);
+	}
+
+	render(ctx) {
+		let pad = this.padding;
+		ctx.fillStyle = this.color;
+		ctx.fillRect(this.x + pad,
+			this.y + pad, 
+			this.width - pad, this.height - pad);
+	}
+}
+
+class Slider extends Brick {
+	constructor(x, y) {
+		super(x, y);
+		this.velocity = Vec2.ZERO();
+		this.padding = 0;
+		this.height = 35;
+	}
+	
+	update () {
+		
+	}
+	
+	render (ctx) {
+		super.render(ctx);
+	}
+}
+
+class Grid extends CanvasObject {
+	rows = 5;
+	columns = 6;
+	bricks = [];
+	constructor() {
+		super(0, 0);
+		for (let row = 0; row < this.rows; row++) { // Start from 0 for positioning
+			for (let col = 0; col < this.columns; col++) {
+				let B = new Brick(0, 0);
+				let positionX = B.width * col; // Adjusted for correct positioning
+				let positionY = B.height * row;
+				B.x = positionX;
+				B.y = positionY;
+				this.bricks.push(B);
+			}
+		}
+	}
+
+	render(ctx) {
+		this.bricks.forEach((b) => b.render(ctx))
+	}
+
+}
+
+canvas = document.getElementById("canvas");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+var gameGrid;
+var player;
+
+function init() {
+	gameGrid = new Grid();
+	player = new Slider(140, 220);
+	player.color = '#c0bfbc'
+}
 
 function draw() {
-  function adjustCanvas() {
-    let canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
+	
+	if (canvas.getContext) {
+		const ctx = canvas.getContext("2d");
+		// Clear context
+		ctx.clearRect(0, 0, 360, 360);
+		gameGrid.render(ctx);
+		player.render(ctx);
+	}
 
-  adjustCanvas();
-
-  // Ajuste le canvas lorsque la fenêtre est redimensionnée
-  window.addEventListener("resize", adjustCanvas);
-
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-
-    // Dessine les briques
-    const brickWidth = window.innerWidth / 25;
-    const brickHeight = window.innerHeight / 25;
-
-    const nbColumns = Math.floor(
-      (canvas.width - canvas.width / 5) / brickWidth
-    );
-    const nbLines = Math.floor(
-      (canvas.height - canvas.height / 1.5) / brickHeight
-    );
-
-    for (let i = 0; i < nbLines; i++) {
-      for (let j = 0; j < nbColumns; j++) {
-        const x = canvas.width / 10 + j * brickWidth;
-        const y = canvas.height / 10 + i * brickHeight;
-
-        if (i % 2 == 0) ctx.fillStyle = "green";
-        else ctx.fillStyle = "yellow";
-        ctx.fillRect(x, y, brickWidth, brickHeight);
-      }
-    }
-  }
 }
-draw();
+init();
+// 16 ms for 60 fps
+setInterval(draw, 16);
