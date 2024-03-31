4639e0217b8165036015711d105736256f6146af
Youssef Sahli <sahli.youssef@ymail.com>
2024-03-31 17:18:06 +0200

    add Input Handler

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -2,75 +2,74 @@
  * Y. Sahli GPLv3
  * Main Canvas File
  */
-import { InputAction } from "./modules/input_action.js";
 import { Vec2 } from "./modules/vec2.js";
 import {makeGradient} from "./modules/utils.js";
 
 class CanvasObject extends Vec2 {
-	// Ajouter hidden ? destroyable ?
-	constructor(x, y) {
-		super(x, y);
-	}
+    // Ajouter hidden ? destroyable ?
+    constructor(x, y) {
+        super(x, y);
+    }
 }
 
 class Brick extends CanvasObject {
-	// TODO Differents types ?
-	width = 71;
-	height = 36;
-	padding = 5;
-	color = makeGradient('#99c1f1', '#1a5fb4');
+    // TODO Differents types ?
+    width = 71;
+    height = 36;
+    padding = 5;
+    color = makeGradient('#99c1f1', '#1a5fb4');
 
-	constructor(x, y) {
-		super(x, y);
-	}
+    constructor(x, y) {
+        super(x, y);
+    }
 
-	render(ctx) {
-		let pad = this.padding;
-		ctx.fillStyle = this.color;
-		ctx.fillRect(this.x + pad,
-			this.y + pad,
-			this.width - pad, this.height - pad);
-	}
+    render(ctx) {
+        let pad = this.padding;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x + pad,
+                this.y + pad,
+                this.width - pad, this.height - pad);
+    }
 }
 
 class Slider extends Brick {
-	constructor(x, y) {
-		super(x, y);
-		this.velocity = Vec2.ZERO();
-		this.padding = 0;
-		this.height = 35;
-	}
+    constructor(x, y) {
+        super(x, y);
+        this.velocity = Vec2.ZERO();
+        this.padding = 0;
+        this.height = 35;
+    }
 
-	update() {
-		// TODO Movement
-	}
+    update() {
+        // TODO Movement
+    }
 
-	render(ctx) {
-		super.render(ctx);
-	}
+    render(ctx) {
+        super.render(ctx);
+    }
 }
 
 class Grid extends CanvasObject {
-	rows = 5;
-	columns = 6;
-	bricks = [];
-	constructor() {
-		super(0, 0);
-		for (let row = 0; row < this.rows; row++) { // Start from 0 for positioning
-			for (let col = 0; col < this.columns; col++) {
-				let B = new Brick(0, 0);
-				let positionX = B.width * col; // Adjusted for correct positioning
-				let positionY = B.height * row;
-				B.x = positionX;
-				B.y = positionY;
-				this.bricks.push(B);
-			}
-		}
-	}
+    rows = 5;
+    columns = 6;
+    bricks = [];
+    constructor() {
+        super(0, 0);
+        for (let row = 0; row < this.rows; row++) { // Start from 0 for positioning
+            for (let col = 0; col < this.columns; col++) {
+                let B = new Brick(0, 0);
+                let positionX = B.width * col; // Adjusted for correct positioning
+                let positionY = B.height * row;
+                B.x = positionX;
+                B.y = positionY;
+                this.bricks.push(B);
+            }
+        }
+    }
 
-	render(ctx) {
-		this.bricks.forEach((b) => b.render(ctx))
-	}
+    render(ctx) {
+        this.bricks.forEach((b) => b.render(ctx))
+    }
 
 }
 
@@ -81,29 +80,29 @@ var gameGrid;
 var player;
 
 function init() {
-	gameGrid = new Grid();
-	player = new Slider(110, 220);
-	player.color = makeGradient('#99c1f1', '#26a269');
+    gameGrid = new Grid();
+    player = new Slider(110, 220);
+    player.color = makeGradient('#99c1f1', '#26a269');
 }
 
 function update() {
-	// called 60 times per second
+    // called 60 times per second
 }
 
 function draw() {
-	// called 60 times per second
-	if (canvas.getContext) {
-		const ctx = canvas.getContext("2d");
-		// Clear context
-		ctx.clearRect(0, 0, 360, 360);
-		gameGrid.render(ctx);
-		player.render(ctx);
-	}
+    // called 60 times per second
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        // Clear context
+        ctx.clearRect(0, 0, 360, 360);
+        gameGrid.render(ctx);
+        player.render(ctx);
+    }
 
 }
 init();
 // 16 ms for 60 fps
 setInterval(() => {
-	update();
-	draw();
+    update();
+    draw();
 }, 16);
--- src/main/webapp/modules/input_action.js
+++ src/main/webapp/modules/input_action.js
@@ -2,16 +2,45 @@
  * InputAction class 
  * see Input handler pattern
  */
-export class InputAction {
-	constructor (name, key, func){
-		this.name = name;
-                this.key = key;
-                this.func = func; // When key pressed, execute async? func
-	}
+
+/**
+ * 
+ * @type InputAction
+ */
+class InputAction {
+    constructor(name, key, func) {
+        this.name = name;
+        this.key = key;
+        this.func = func; // When key pressed, execute async? func
+    }
 }
 
+/**
+ * 
+ * @type ActionMap
+ */
+class ActionMap extends Map {
+    constructor (kv) {
+        super(kv);
+    }
+    /**
+     * 
+     * @param {InputAction} i
+     * @returns {ActionMap}
+     */
+    register (i) {
+        return super.set(i.key, i.func);
+    }
+    
+}
+
+
+/**
+ * 
+ * @type InputHandler
+ */
 export class InputHandler {
-    constructor () {
+    constructor() {
         
     }
 }
