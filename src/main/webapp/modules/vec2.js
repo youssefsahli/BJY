2368b94a278a81b793ed1a563405cdb38fdd1fac
Youssef Sahli <sahli.youssef@ymail.com>
2024-04-01 01:45:12 +0200

    better vectors

--- src/main/webapp/modules/vec2.js
+++ src/main/webapp/modules/vec2.js
@@ -5,13 +5,12 @@ export class Vec2 {
         this.x = x;
         this.y = y;
     }
-    static ZERO  = new Vec2( 0,  0)
-    static ONE   = new Vec2( 1,  1)
-    static LEFT  = new Vec2(-1,  0)
-    static RIGHT = new Vec2( 1,  0)
-    static UP    = new Vec2( 0, -1)
-    static DOWN  = new Vec2( 0,  1)
-
+    static ZERO = new Vec2(0, 0)
+    static ONE = new Vec2(1, 1)
+    static LEFT = new Vec2(-1, 0)
+    static RIGHT = new Vec2(1, 0)
+    static UP = new Vec2(0, -1)
+    static DOWN = new Vec2(0, 1)
 
     times(n) {
         return new Vec2(this.x * n, this.y * n);
@@ -28,40 +27,39 @@ export class Vec2 {
     sub(v) {
         return new Vec2(this.x - v.x, this.y - v.y);
     }
-    
-    len () {
-        return Math.sqrt((this.x ** 2, this.y ** 2));
+
+    len() {
+        return Math.sqrt((this.x ** 2 + this.y ** 2));
     }
-    
-    norm () {
+
+    norm() {
         return this.divide(this.len());
     }
-    
-    get angle () {
-        Math.atan2(this.y, this.x);
+
+    get angle() {
+        return Math.atan2(this.y, this.x);
     }
-    
-    set angle (beta) {
-        let x = Math.cos(beta*this.x) - Math.sin(beta*this.y);
-        let y = Math.sin(beta*this.x) + Math.cos(beta*this.y);
-        this.x = x;
-        this.y = y;
+
+    set angle(beta) {
+        this.x = Math.cos(beta) * this.len();
+        this.y = Math.sin(beta) * this.len();
     }
-    
-    dot (v) {
-        return this.x*v.x + this.y*v.y;
+
+    dot(v) {
+        return this.x * v.x + this.y * v.y;
     }
-    
-    perp () {
+
+    perp() {
         return new Vec2(-this.y, this.x);
     }
-    
-    bounce (normal) {
-        return this.sub(normal.times(2*this.dot(normal)));
+
+    bounce(normal) {
+        return this.sub(normal.times(2 * this.dot(normal)));
     }
-    
-    noise (value) {
+
+    noise(value) {
         this.angle += rand(-value, value);
+        return this;
     }
 
 }
